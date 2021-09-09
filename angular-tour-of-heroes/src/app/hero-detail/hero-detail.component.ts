c303e1f13423faf1739ad50e0eaa3e559e85b648
ifranaiyubali <ifran.ali2@dhl.com>
2021-09-10 08:48:52 +1000

    Final Angular Heroes

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero?: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero() {
+    const id  = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHeroNo404(id).subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
+}
