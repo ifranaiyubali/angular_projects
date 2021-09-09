3db6206d50cac287b3ee2fd30759c4a2066755ea
ifranaiyubali <ifran.ali2@dhl.com>
2021-09-10 08:48:52 +1000

    Final Angular Heros

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice', power:'', alterEgo: '' },
+    { id: 12, name: 'Narco', power:'', alterEgo: '' },
+    { id: 13, name: 'Bombasto', power:'', alterEgo: '' },
+    { id: 14, name: 'Celeritas', power:'', alterEgo: '' },
+    { id: 15, name: 'Magneta', power:'', alterEgo: '' },
+    { id: 16, name: 'RubberMan', power:'', alterEgo: '' },
+    { id: 17, name: 'Dynama', power:'', alterEgo: '' },
+    { id: 18, name: 'Dr IQ', power:'', alterEgo: '' },
+    { id: 19, name: 'Magma', power:'', alterEgo: '' },
+    { id: 20, name: 'Tornado', power:'', alterEgo: '' }
+  ];
