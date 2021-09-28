6694ff44004696ec7fc6620c4f171ad40933aca2
ifranaiyubali <ifran.ali2@dhl.com>
2021-09-28 18:48:20 +1000

    Promise Part 1

--- angular_course/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular_course/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -23,7 +23,8 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     let id = this.route.snapshot.params['id'];
     console.log(id)
-    this.dish = this.dishservice.getDish(id);
+    this.dishservice.getDish(id)
+    .then((dish)=> this.dish = dish);
   }
 
   goBack(): void {
