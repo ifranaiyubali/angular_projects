059b581aca3afa13db5e963afdda74d6426c5811
ifranaiyubali <ifran.ali2@dhl.com>
2021-09-28 23:31:10 +1000

    RxJs Part 1

--- angular_course/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular_course/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -24,7 +24,7 @@ export class DishdetailComponent implements OnInit {
     let id = this.route.snapshot.params['id'];
     console.log(id)
     this.dishservice.getDish(id)
-    .then((dish)=> this.dish = dish);
+    .subscribe((dish)=> this.dish = dish);
   }
 
   goBack(): void {
