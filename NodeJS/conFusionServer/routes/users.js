8ffe17a7660fffd7b55876e88ebe5a16c8a7ea24
ifranaiyubali <ifran.ali2@dhl.com>
2021-10-27 09:53:10 +1000

    CORS

--- NodeJS/conFusionServer/routes/users.js
+++ NodeJS/conFusionServer/routes/users.js
@@ -10,7 +10,7 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
   // res.send('respond with a resource');
   User.find({})
     .then(
@@ -23,7 +23,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req,
     .catch((err) => next(err));
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
@@ -41,14 +41,14 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions,(req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
