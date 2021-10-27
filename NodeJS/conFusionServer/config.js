9e4460becd1b6d3c1ee379cf5061e6c5788279f5
ifranaiyubali <ifran.ali2@dhl.com>
2021-10-27 12:20:04 +1000

    Passport Facebook

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '379981217196151',
+        clientSecret: 'd905337652efda8f8f715c12795a3e82'
+    }
 }
